641b35835d5b928055b34dbe9e33ef48af02698f
Nagireddy <anakalanagireddy@gmail.com>
2022-10-27 11:20:07 +0530

    My first commit

---
+++ src/main/java/com/demo/controller/StudentController.java
@@ -0,0 +1,19 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+	
+	@GetMapping("/marks")
+	public String marks()
+	{
+		
+		
+		return "Nagireddy";
+		
+	}
+}
